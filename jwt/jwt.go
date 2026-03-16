3c6ad8d6b7c95ad5782077e354b0aa4891a14acc
GunarsK <gunarskunakovs@gmail.com>
2026-03-16 12:05:42 +0200

    Generalize messaging models for multi-type emails
    - Rename ContactMessage to Email with type and recipient_email fields
    - Add email and email_verified to JWT claims
    - Add ResourceEmails constant for S2S scope checking
    - Upgrade Go to 1.26.1 and update all dependencies

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -20,9 +20,11 @@ var (
 
 // Claims represents JWT token claims with user information.
 type Claims struct {
-	UserID   int64             `json:"user_id"`
-	Username string            `json:"username"`
-	Scopes   map[string]string `json:"scopes,omitempty"`
+	UserID        int64             `json:"user_id"`
+	Username      string            `json:"username"`
+	Email         string            `json:"email,omitempty"`
+	EmailVerified bool              `json:"email_verified,omitempty"`
+	Scopes        map[string]string `json:"scopes,omitempty"`
 	jwt.RegisteredClaims
 }
 
